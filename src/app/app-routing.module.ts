b16fb6efbae94938864a8a3ae0cd95ba65f34ba0
Jason Huang <jason.hts0817@gmail.com>
2023-10-29 08:46:23 +0800

    feat: add sand-box component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sand-box',
+    title: '測試區',
+    loadComponent: () =>
+      import('./pages/sand-box/sand-box.component').then(
+        (mod) => mod.SandBoxComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HttpClientModule],
---
+++ src/app/pages/sand-box/sand-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sand-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sand-box.component.html',
+  styleUrls: ['./sand-box.component.scss']
+})
+export class SandBoxComponent {
+
+}
