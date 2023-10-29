08bbe0ccb5249a276adc59074592c81e587dd477
Jason Huang <jason.hts0817@gmail.com>
2023-10-29 17:01:37 +0800

    feat: add environment

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -7,6 +7,7 @@ import { Elctks } from '../models/elctks.model';
 import { Elbase } from '../models/elbase.model';
 import { Elcand } from '../models/elcand.model';
 import { Elpaty } from '../models/elpaty.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -17,31 +18,31 @@ export class ApiService {
   /** 行政區基本資料 */
   getElbase(year: VoteYearEnum) {
     return this.http
-      .get<Elbase[]>(`/assets/api/president/${year}/elbase.json`)
+      .get<Elbase[]>(`${environment.apiUrl}/api/president/${year}/elbase.json`)
       .pipe(map((datas) => datas.map((data) => new Elbase(data))));
   }
   /** 候選人得票 */
   getElcand(year: VoteYearEnum) {
     return this.http
-      .get<Elcand[]>(`/assets/api/president/${year}/elcand.json`)
+      .get<Elcand[]>(`${environment.apiUrl}/api/president/${year}/elcand.json`)
       .pipe(map((datas) => datas.map((data) => new Elcand(data))));
   }
   /** 政黨基本資料 */
   getElpaty(year: VoteYearEnum) {
     return this.http
-      .get<Elpaty[]>(`/assets/api/president/${year}/elpaty.json`)
+      .get<Elpaty[]>(`${environment.apiUrl}/api/president/${year}/elpaty.json`)
       .pipe(map((datas) => datas.map((data) => new Elpaty(data))));
   }
   /** 取得選舉概況 */
   getElprof(year: VoteYearEnum) {
     return this.http
-      .get<Elprof[]>(`/assets/api/president/${year}/elprof.json`)
+      .get<Elprof[]>(`${environment.apiUrl}/api/president/${year}/elprof.json`)
       .pipe(map((datas) => datas.map((data) => new Elprof(data))));
   }
   /** 取得選舉概況 */
   getElctks(year: VoteYearEnum) {
     return this.http
-      .get<Elctks[]>(`/assets/api/president/${year}/elctks.json`)
+      .get<Elctks[]>(`${environment.apiUrl}/api/president/${year}/elctks.json`)
       .pipe(map((datas) => datas.map((data) => new Elctks(data))));
   }
 }
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: '/the-f2e/assets',
+};
