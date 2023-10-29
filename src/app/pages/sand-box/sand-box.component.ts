d71eea2976297c02c5bd1032c3e0f68080d6afb4
Jason Huang <jason.hts0817@gmail.com>
2023-10-29 08:53:26 +0800

    plugin: install tailwind

--- src/app/pages/sand-box/sand-box.component.ts
+++ src/app/pages/sand-box/sand-box.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiService } from 'src/app/core/service/api.service';
+import { VoteYearEnum } from 'src/app/core/enums/vote-year.enum';
 
 @Component({
   selector: 'app-sand-box',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './sand-box.component.html',
-  styleUrls: ['./sand-box.component.scss']
+  styleUrls: ['./sand-box.component.scss'],
 })
-export class SandBoxComponent {
+export class SandBoxComponent implements OnInit {
+  VoteYearEnum = VoteYearEnum;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    // this.apiService.getElbase()
+  }
 }
