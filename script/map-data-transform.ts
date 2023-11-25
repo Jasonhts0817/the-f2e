2d713160f53dc64ec4f9a883e85af5efadddd4cd
Jason Huang <jason.hts0817@gmail.com>
2023-11-25 21:38:38 +0800

    feat: add 1982 map data

--- script/map-data-transform.ts
+++ script/map-data-transform.ts
@@ -1,54 +1,65 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as csv from 'csv-parser';
 
-const DATA_DIR_PATH = path.join(`${__dirname}/..`, 'data');
-const DATA_FILE_EXT = '.csv';
-const API_DIR_PATH = path.join(`${__dirname}/..`, 'src/api');
-
-function getDataFilePath(folderPath: string): string[] {
-  let filePathList: string[] = [];
-  const files = fs.readdirSync(folderPath);
-
-  files.forEach((file) => {
-    const filePath = path.join(folderPath, file);
-    const fileStat = fs.statSync(filePath);
-
-    switch (true) {
-      case fileStat.isDirectory():
-        filePathList = [...filePathList, ...getDataFilePath(filePath)];
-        break;
-      case fileStat.isFile():
-        if (DATA_FILE_EXT === path.parse(file).ext) {
-          filePathList.push(filePath);
-        }
-        break;
-    }
-  });
-  return filePathList;
+const DATA_PATH = path.join(`${__dirname}/..`, 'data/map/twTown1982.topo.json');
+const ELBASE_DATA_PATH = path.join(
+  `${__dirname}/..`,
+  'src/api/president/1996/elbase.json',
+);
+const API_DIR_PATH = path.join(`${__dirname}/..`, 'src/api/map/1982/towns');
+function getCountryObj() {
+  const elbaseRawData = fs.readFileSync(ELBASE_DATA_PATH).toString();
+  const elbaseData = JSON.parse(elbaseRawData);
+  return elbaseData
+    .filter((elbase: any) => elbase[3] === '000' && elbase[4] === '0000')
+    .reduce((countryObj: { [key: string]: string }, elbase: any) => {
+      countryObj[elbase[5]] = `${elbase[0]}${elbase[1]}`;
+      return countryObj;
+    }, {});
 }
-
-function convertCSVtoJson(dataFilePath: string) {
-  const results: any = [];
-  fs.createReadStream(dataFilePath)
-    .pipe(csv({ headers: false }))
-    .on('data', (data) => {
-      results.push(data);
-    })
-    .on('end', () => {
-      const jsonString = JSON.stringify(results, null, 2);
-      const { base, name } = path.parse(dataFilePath);
-      const subPath = dataFilePath.replace(DATA_DIR_PATH, '').replace(base, '');
-      const apiFilePath = `${API_DIR_PATH}${subPath}`;
-      fs.mkdirSync(apiFilePath, { recursive: true });
-      fs.writeFileSync(`${apiFilePath}${name}.json`, jsonString);
-    });
-}
-
 function dataTransform() {
-  const dataFilePaths = getDataFilePath(DATA_DIR_PATH);
-  dataFilePaths.forEach((dataFilePath) => {
-    convertCSVtoJson(dataFilePath);
+  const mapRawData = fs.readFileSync(DATA_PATH).toString();
+  const mapData = JSON.parse(mapRawData);
+  const countrObj = getCountryObj();
+  const defaultMapData = {
+    type: mapData.type,
+    transform: mapData.transform,
+    objects: {
+      map: {
+        type: mapData.objects.layer1.type,
+        geometries: [],
+        bbox: mapData.objects.layer1.bbox,
+      },
+    },
+    arcs: mapData.arcs,
+    bbox: mapData.bbox,
+  };
+  const countrysTopoJson: { [key: string]: any } = {};
+  mapData.objects.layer1.geometries.forEach((geo: any) => {
+    const { properties } = geo;
+    const countryId: string = countrObj[properties.COUNTYNAME];
+
+    if (!countrysTopoJson[countryId]) {
+      countrysTopoJson[countryId] = JSON.parse(JSON.stringify(defaultMapData));
+    }
+
+    const geometry = {
+      ...geo,
+      properties: {
+        id: properties.TOWNID,
+        name: properties.TOWNNAME,
+      },
+    };
+    countrysTopoJson[countryId].objects.map.geometries.push(geometry);
   });
+
+  Object.keys(countrysTopoJson).map((countryId) => {
+    fs.writeFileSync(
+      `${API_DIR_PATH}/towns-${countryId}.json`,
+      JSON.stringify(countrysTopoJson[countryId]),
+    );
+  });
+  console.log(countrObj);
 }
+
 dataTransform();
